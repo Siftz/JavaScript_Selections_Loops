73dde227525e485f26e6354d3eaad760fd00d95c
Logan Akins <lakins2@gmail.com>
2025-05-08 16:12:45 -0400

    Exercise 5 added adnd completed

--- app.js
+++ app.js
@@ -68,6 +68,25 @@ do {
     }
     i++;
 } while (i <= 100);
+//exercise 5 bonus
+/*console.log("EXERCISE 5 BONUS for 2 Custom:\n==========\n");
+let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let start = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let end = Math.round(Math.random() * (1000 - 100) + 100); // creates a random number between 100 and 1000
+
+console.log(`Using fizzDivisor: ${fizzDivisor}, buzzDivisor: ${buzzDivisor}`);
+console.log(`Loop starts at: ${start}, ends at: ${end}`);
+//loop from start to end
+for (let i = start; i <= end; i++) {
+    if (i % fizzDivisor == 0) {
+        console.log("FIZZBUZZ");
+    } else if (i % fizzDivisor == 0) {
+        console.log("FIZZ");
+    } else if (i % buzzDivisor == 0) {
+        console.log("BUZZ");
+    }
+}*/
 
 // exercise 4
 console.log("EXERCISE 4: \n=========\n");
@@ -84,4 +103,23 @@ for (let i = 0; i <=n; i++) {
 }
 if (!found) {
     console.log("DId not find value.");
+}
+//exercise 5 bonus
+console.log("EXERCISE 5 BONUS for 2 Custom:\n==========\n");
+let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let start = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let end = Math.round(Math.random() * (1000 - 100) + 100); // creates a random number between 100 and 1000
+
+console.log(`Using fizzDivisor: ${fizzDivisor}, buzzDivisor: ${buzzDivisor}`);
+console.log(`Loop starts at: ${start}, ends at: ${end}`);
+//loop from start to end
+for (let i = start; i <= end; i++) {
+    if (i % fizzDivisor == 0) {
+        console.log("FIZZBUZZ");
+    } else if (i % fizzDivisor == 0) {
+        console.log("FIZZ");
+    } else if (i % buzzDivisor == 0) {
+        console.log("BUZZ");
+    }
 }
